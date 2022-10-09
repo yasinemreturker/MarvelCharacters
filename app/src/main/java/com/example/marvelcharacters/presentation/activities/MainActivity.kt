d0fc37eb35b6264d8429af26173f3f7e6c8b2356
byTurker <mr_turker@hotmail.com>
2022-10-10 00:08:39 +0300

    Add project package structure

--- app/src/main/java/com/example/marvelcharacters/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/marvelcharacters/presentation/activities/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.marvelcharacters
+package com.example.marvelcharacters.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marvelcharacters.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
