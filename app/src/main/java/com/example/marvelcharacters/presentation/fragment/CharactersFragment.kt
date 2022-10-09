e540dde803b232ff72ddf6de3128cfd545fb759e
byTurker <mr_turker@hotmail.com>
2022-10-10 02:28:32 +0300

    Fragments path fix

--- app/src/main/java/com/example/marvelcharacters/presentation/fragment/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelcharacters/presentation/fragment/CharactersFragment.kt
@@ -1,4 +1,4 @@
-package com.example.marvelcharacters.presentation.fragments
+package com.example.marvelcharacters.presentation.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.marvelcharacters.R
 
-class SearchFragment : Fragment() {
+class CharactersFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return inflater.inflate(R.layout.fragment_characters, container, false)
     }
+
 
 }
--- app/src/main/java/com/example/marvelcharacters/presentation/fragment/DetailsFragment.kt
+++ app/src/main/java/com/example/marvelcharacters/presentation/fragment/DetailsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.marvelcharacters.presentation.fragments
+package com.example.marvelcharacters.presentation.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,15 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.marvelcharacters.R
 
-class CharactersFragment : Fragment() {
+class DetailsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+        return inflater.inflate(R.layout.fragment_details, container, false)
     }
-
 
 }
